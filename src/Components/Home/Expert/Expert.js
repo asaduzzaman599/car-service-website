17718f19281598ea1a7c136641f4245764cd41d3
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-11 17:52:30 +0600

    update rating component

--- src/Components/Home/Expert/Expert.js
+++ src/Components/Home/Expert/Expert.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import Rating from 'react-rating';
+import RatingComponent from '../../Shared/RatingComponent/RatingComponent';
 
 const Expert = ({expert}) => {
     const {name,rating,img}=expert
     return (
         <Col md={3} lg={4}>
 
-            <Card style={{ width: '18rem' }}>
+            <Card className='d-flex' style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={img} />
+                <Card.ImgOverlay></Card.ImgOverlay>
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
-                    <Card.Text>{rating}</Card.Text>
+                   <RatingComponent rating={rating}></RatingComponent>
                     
                     {/* <Button variant="primary">Go somewhere</Button> */}
                 </Card.Body>
